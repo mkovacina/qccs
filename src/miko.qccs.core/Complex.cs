e5d12e1992fea01d2b0d3e23ad66f1b40b0bfda8
Mike Kovacina <michael.kovacina@gmail.com>
2023-04-14 10:06:29 -0400

    The library for the core logic

---
+++ src/miko.qccs.core/Complex.cs
@@ -0,0 +1,11 @@
+﻿namespace miko.qccs.core;
+public class Complex
+{
+    public double Real {get;}
+    public double Imaginary {get;}
+    public Complex(double real, double imaginary)
+    {
+       Real = real;
+       Imaginary = imaginary; 
+    }
+}
