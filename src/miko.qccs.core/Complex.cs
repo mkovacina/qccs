3cc6899feda55b9113c8c7a4cb0a82b679cc5d34
Mike Kovacina <michael.kovacina@gmail.com>
2023-04-17 10:07:52 -0400

    Adding two numbers...behold

--- src/miko.qccs.core/Complex.cs
+++ src/miko.qccs.core/Complex.cs
@@ -8,4 +8,14 @@ public class Complex
        Real = real;
        Imaginary = imaginary; 
     }
+
+    public static Complex Add(Complex c1, Complex c2)
+    {
+        double real = c1.Real+c2.Real;
+        double imaginary = c1.Imaginary + c2.Imaginary;
+
+        var result = new Complex(real,imaginary);
+
+        return result;
+    }
 }
--- test/miko.qccs.tests/ComplexTests.cs
+++ test/miko.qccs.tests/ComplexTests.cs
@@ -14,4 +14,21 @@ public class ComplexTests
         Assert.That(x.Real, Is.EqualTo(realPart));
         Assert.That(x.Imaginary, Is.EqualTo(imaginaryPart));
     }
+
+    [TestCase(0,0,0,0,0,0)]
+    [TestCase(1,2,3,4,4,6)]
+    [TestCase(-1,-2,3,4,2,2)]
+    [TestCase(.1,.2,.3,.4,.4,.6)]
+    public void Test_Add(double realPart1, double imaginaryPart1, double realPart2, double imaginaryPart2, double expectedRealPart, double expectedImaginaryPart)
+    {
+        var x1 = new Complex(realPart1, imaginaryPart1);
+        var x2 = new Complex(realPart2, imaginaryPart2);
+
+        var expected = new Complex(expectedRealPart, expectedImaginaryPart);
+
+        var actual = Complex.Add(x1,x2);
+
+        Assert.That(actual.Real, Is.EqualTo(expected.Real));
+        Assert.That(actual.Imaginary, Is.EqualTo(expected.Imaginary).Within(1).Ulps);
+    }
 }
